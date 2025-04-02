8a385cd9eeb3040951efa74a91ab97a673e2b271
ifdc2308 <italodorea123@gmail.com>
2025-04-01 22:38:53 -0300

    grid

--- src/pages/dashboard/controlPanel/ControlPanel.tsx
+++ src/pages/dashboard/controlPanel/ControlPanel.tsx
@@ -1,13 +1,25 @@
 'use client'
 import { Navbar } from "../../../components/Navbar";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 
 export const ControlPanel = () => {
-  
-  return (
-    <>
-      <Navbar>
-      <p>teste</p>
-      </Navbar>
-    </>
-  );
+
+    return (
+        <>
+            <Navbar>
+                <Grid templateColumns="1fr 2fr" gap={6} p={3}>
+                    <GridItem>
+                        <Box w='100%' h='10' border={"1px solid"} borderColor={"#d0d0d0"} borderRadius={10} height={"80vh"}>
+                            <p>clientes</p>
+                        </Box>
+                    </GridItem>
+                    <GridItem>
+                        <Box w='100%' h='10' border={"1px solid"} borderColor={"#d0d0d0"} borderRadius={10} height={"80vh"}>
+                            <p>FT / Atividades</p>
+                        </Box>
+                    </GridItem>
+                </Grid>
+            </Navbar>
+        </>
+    );
 };
