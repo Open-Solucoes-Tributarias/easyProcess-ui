773456249a9ed5ee56f0530584b7cff71c07ce1d
ifdc2308 <italodorea123@gmail.com>
2025-04-21 21:03:51 -0300

    clients-get

--- src/pages/controlPanel/sections/Clients.tsx
+++ src/pages/controlPanel/sections/Clients.tsx
@@ -1,40 +1,69 @@
-"use client";
+'use client'
 
-import { Grid, GridItem, List, ListItem } from "@chakra-ui/react";
-import { SearchInput } from "../../../components/InputSearch";
-
-const clients = [
-  "TechPlus Solutions",
-  "Alpha Contabilidade",
-  "Green Agro",
-  "Nova Saúde",
-  "LogiTrans",
-];
+import { useEffect, useState } from 'react'
+import { Grid, GridItem, List, ListItem, Text } from '@chakra-ui/react'
+import { SearchInput } from '../../../components/InputSearch'
+import { getEmpresas, Empresa } from '../../../services/companyService'
 
 export const Clients = () => {
+  const [empresas, setEmpresas] = useState<Empresa[]>([])
+  const [loading, setLoading] = useState(true)
+
+  //capturar empresa clicada
+  const handleEmpresa = (empresa: Empresa) => {
+    alert(`Empresa selecionada: ${empresa.nome}`)
+    console.log("Nome da empresa selecionada:", empresa.nome)
+  }
+
+  useEffect(() => {
+    const fetchEmpresas = async () => {
+      try {
+        const data = await getEmpresas()
+        setEmpresas(data)
+      } catch (error) {
+        console.error('Erro ao carregar empresas:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchEmpresas()
+  }, [])
+
   return (
     <Grid templateColumns="1fr" gap={6} p={3}>
       <GridItem>
         <SearchInput />
       </GridItem>
+
       <GridItem pb={4} paddingInline={5}>
-        <List styleType="disc" spacing={3}>
-          {clients.map((client, index) => (
-            <ListItem sx={styles.listItem} key={index}>
-              {client}
-            </ListItem>
-          ))}
-        </List>
+        {loading ? (
+          <Text fontStyle="italic" color="gray.500">
+            Carregando empresas...
+          </Text>
+        ) : empresas.length === 0 ? (
+          <Text color="gray.500">Nenhuma empresa encontrada.</Text>
+        ) : (
+          <List styleType="disc" spacing={3}>
+            {empresas.map((empresa) => (
+              <ListItem sx={styles.listItem} key={empresa.id} onClick={() => {
+                handleEmpresa(empresa);
+              }}>
+                {empresa.nome}
+              </ListItem>
+            ))}
+          </List>
+        )}
       </GridItem>
     </Grid>
-  );
-};
+  )
+}
 
 const styles = {
   listItem: {
     _hover: {
-      color: "#68D391",
-      cursor: "pointer",
+      color: '#68D391',
+      cursor: 'pointer',
     },
   },
-};
+}
